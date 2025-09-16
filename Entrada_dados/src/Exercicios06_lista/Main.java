4dd27bca17188f0366140ab9040decfefb56bd05
Leandro Figueiredo <Leeeandrofigueiredo@gmail.com>
2025-09-16 08:42:40 -0300

    Exercício 6

---
+++ Entrada_dados/src/Exercicios06_lista/Main.java
@@ -0,0 +1,34 @@
+package Exercicios06_lista;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Double A = sc.nextDouble();
+		Double B = sc.nextDouble();
+		Double C = sc.nextDouble();
+		
+		double areatriangulo = (A * C)/2;
+		double areacirculo = 3.14159 * (Math.pow(C, 2));
+		double areatrapezio = ((A + B)*C)/2;
+		double areaquadrado = B*B;
+		double arearetangulo = (A * B);
+		
+		System.out.printf("%n%nTRIANGULO R$ %.3f", areatriangulo);
+		System.out.printf("%n%nCIRCULO R$ %.3f", areacirculo);
+		System.out.printf("%n%nTRAPEZIO %.3f", areatrapezio);
+		System.out.printf("%n%nTRAPEZIO %.3f", areaquadrado);
+		System.out.printf("%n%nTRAPEZIO %.3f", arearetangulo);
+		
+		sc.close();
+		
+	}
+	
+}

a10e8e3b2acc88ebe0dc267b5227d28326820b4a
Leandro Figueiredo <Leeeandrofigueiredo@gmail.com>
2025-10-17 20:46:57 -0300

    exercio repeticao e debug

--- Entrada_dados/src/Escopo_inicializacao/Main.java
+++ Entrada_dados/src/Escopo_inicializacao/Main.java
@@ -1,19 +1,24 @@
 package Escopo_inicializacao;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner (System.in);
+		Locale.setDefault(Locale.US);
 		
-		double preco = 34.5;
-		double desconto = (preco < 20 )? preco * 0.1 : preco *0.05;
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println(desconto);
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", preco);
+		
 		sc.close();
-	
 	}
-
 }
